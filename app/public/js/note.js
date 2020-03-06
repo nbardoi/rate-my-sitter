586e7afa4bcdf6e87bc38673f082e70b8f5449a9
Byanca De la fuente <byancadlf@gmail.com>
2020-03-05 19:44:03 -0600

    added

---
+++ app/public/js/note.js
@@ -0,0 +1,119 @@
+var dt = new Date();
+document.getElementById("datetime").innerHTML = moment().format("h:mm");
+
+$(document).ready(function() {
+  var noteContainer = $(".note-container");
+  var postCategorySelect = $("#category");
+
+  $(document).on("click", "button.delete", handlePostDelete);
+  $(document).on("click", "button.edit", handlePostEdit);
+  postCategorySelect.on("change", handleCategoryChange);
+  var posts;
+
+  function getPosts(category) {
+    var categoryString = category || "";
+    if (categoryString) {
+      categoryString = "/category/" + categoryString;
+    }
+    $.get("/api/posts" + categoryString, function(data) {
+      console.log("Posts", data);
+      posts = data;
+      if (!posts || !posts.length) {
+        displayEmpty();
+      } else {
+        initializeRows();
+      }
+    });
+  }
+
+  function deletePost(id) {
+    $.ajax({
+      method: "DELETE",
+      url: "/api/posts/" + id
+    }).then(function() {
+      getPosts(postCategorySelect.val());
+    });
+  }
+
+  getPosts();
+
+  function initializeRows() {
+    noteContainer.empty();
+    var postsToAdd = [];
+    for (var i = 0; i < posts.length; i++) {
+      postsToAdd.push(createNewRow(posts[i]));
+    }
+    noteContainer.append(postsToAdd);
+  }
+
+  function createNewRow(post) {
+    var newPostCard = $("<div>");
+    newPostCard.addClass("card");
+    var newPostCardHeading = $("<div>");
+    newPostCardHeading.addClass("card-header");
+    var deleteBtn = $("<button>");
+    deleteBtn.text("x");
+    deleteBtn.addClass("delete btn btn-secondary");
+    var editBtn = $("<button>");
+    editBtn.text("EDIT");
+    editBtn.addClass("edit btn btn-default");
+    var newPostTitle = $("<h4>");
+    var newPostDate = $("<small>");
+    var newPostCategory = $("<p>");
+    newPostCategory.text(post.category);
+    newPostCategory.css({
+      float: "right",
+      "font-weight": "700",
+      "margin-top": "-15px"
+    });
+    var newPostCardBody = $("<div>");
+    newPostCardBody.addClass("card-body");
+    var newPostBody = $("<p>");
+    newPostTitle.text(post.title + " ");
+    newPostBody.text(post.body);
+    var formattedDate = new Date(post.createdAt);
+    formattedDate = moment(formattedDate).format("MMMM Do YYYY");
+    var newPostDate = $("<p>");
+    newPostDate.text(formattedDate);
+    newPostDate.addClass("date-container");
+    newPostTitle.append(newPostDate);
+    newPostCardHeading.append(deleteBtn);
+    newPostCardHeading.append(editBtn);
+    newPostCardHeading.append(newPostTitle);
+    newPostCardHeading.append(newPostCategory);
+    newPostCardBody.append(newPostBody);
+    newPostCard.append(newPostCardHeading);
+    newPostCard.append(newPostCardBody);
+    newPostCard.data("post", post);
+    return newPostCard;
+  }
+
+  function handlePostDelete() {
+    var currentPost = $(this)
+      .parent()
+      .parent()
+      .data("post");
+    deletePost(currentPost.id);
+  }
+
+  function handlePostEdit() {
+    var currentPost = $(this)
+      .parent()
+      .parent()
+      .data("post");
+    window.location.href = "/cms?post_id=" + currentPost.id;
+  }
+
+  function displayEmpty() {
+    noteContainer.empty();
+    var messageH2 = $("<h3>");
+    messageH2.css({ "text-align": "center", "margin-top": "50px" });
+    messageH2.html("Hello, what would you like to note today?");
+    noteContainer.append(messageH2);
+  }
+
+  function handleCategoryChange() {
+    var newPostCategory = $(this).val();
+    getPosts(newPostCategory);
+  }
+});
