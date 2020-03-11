6461d116d2eb9ea3ec3f5998031ed7871382fe7a
jmike2689 <jmike2689@yahoo.com>
2020-03-10 19:13:13 -0500

    Added About Us

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -5,6 +5,7 @@ module.exports = function(app, passport) {
   app.get("/signin", authController.signin);
   app.get("/", authController.landing);
   app.get("/review", authController.review);
+  app.get("/about", authController.about);
 
   app.post(
     "/signup",
