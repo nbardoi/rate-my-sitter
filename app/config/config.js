e662434eaa8763103eb2e798b7fe7ac72bab2611
Nagma Bardoi <nagmabardoi@gmail.com>
2020-03-10 20:45:20 -0500

    new

--- app/config/config.js
+++ app/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "root",
-    password: "utsabootcamp1",
-    database: "sequelize_passport",
-    host: "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
     dialect: "mysql"
   },
   test: {
