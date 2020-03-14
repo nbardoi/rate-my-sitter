fbafb68060e34c5c4e682b4fa22729603f7bd5d8
jmike2689 <54385882+jmike2689@users.noreply.github.com>
2020-03-14 11:28:01 -0500

    Revert "Dev2"

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -10,7 +10,7 @@ module.exports = function(app, passport) {
   app.post(
     "/signup",
     passport.authenticate("local-signup", {
-      successRedirect: "/signin",
+      successRedirect: "/dashboard",
 
       failureRedirect: "/signup"
     })
