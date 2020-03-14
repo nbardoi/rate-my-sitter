a94bf0f82e74ee99ff9411e81fb45e7779f80134
Byanca De la fuente <byancadlf@gmail.com>
2020-03-14 10:33:25 -0500

    added react and css

---
+++ app/components/nav.jsx
@@ -0,0 +1,34 @@
+const React = require("react");
+
+const Navbar = props => {
+  const selectedOption = props.selectedOption;
+  //   console.log(selectedOption);
+  return (
+    <div>
+      <ul id="nav">
+        <li className={selectedOption === "about-us" ? "active" : ""}>
+          <a href="#" onClick={props.handleClick("about-us")}>
+            About
+          </a>
+        </li>
+        <li className={selectedOption === "team" ? "active" : ""}>
+          <a href="#" onClick={props.handleClick("team")}>
+            The team
+          </a>
+        </li>
+        <li className={selectedOption === "FAQ" ? "active" : ""}>
+          <a href="#" onClick={props.handleClick("FAQ")}>
+            FAQ
+          </a>
+        </li>
+        <li className={selectedOption === "contact" ? "active" : ""}>
+          <a href="#" onClick={props.handleClick("contact")}>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+module.exports = Navbar;
